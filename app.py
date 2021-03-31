07133796e1666fad6ff3034155afafbf7d73151e
Eddie Shin <eshinhw@gmail.com>
2021-03-30 23:46:09 -0400

    database added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from tkinter import ttk
 import tkinter as tk
 from tkinter import filedialog, Text
 import os
@@ -6,15 +7,51 @@ import sqlite3
 root = tk.Tk()
 root.title("Stock Price Alert")
 root.geometry("800x800")
-def journal():
-    symbol = symbol_input.get()
-    price = price_input.get()
 
-    sp = tk.Label(root, text=f"You entered {symbol} at {price}")
-    sp.pack()
+def add():
 
-# canvas = tk.Canvas(root, height=500, width=500, bg="#263D42")
-# canvas.pack()
+    conn = sqlite3.connect('price_alert.db')
+    c = conn.cursor()
+
+    # Create a table
+
+    c.execute("""CREATE TABLE IF NOT EXISTS prices_v2 (
+        symbol text,
+        target_price real
+        ) """
+              )
+
+    # Insert into table
+
+    c.execute("INSERT INTO prices_v2 VALUES (:symbol, :target_price)",
+              {
+                  'symbol': symbol_input.get(),
+                  'target_price': price_input.get()
+              })
+
+    symbol_input.delete(0, tk.END)
+    price_input.delete(0, tk.END)
+    # Commit changes
+    conn.commit()
+
+    # Close connection
+    conn.close()
+
+def query():
+    conn = sqlite3.connect('price_alert.db')
+    c = conn.cursor()
+
+    c.execute("SELECT *, oid FROM prices_v2")
+    records = c.fetchall() # fetches all records
+
+    for record in records:
+        print(record)
+        tree.insert("", tk.END, values=record)
+
+    # Close connection
+    conn.close()
+
+
 
 symbol = tk.Label(root, text="Symbol: ")
 symbol.grid(row=0,column=0)
@@ -26,57 +63,23 @@ target_price.grid(row=0,column=2)
 price_input = tk.Entry()
 price_input.grid(row=0,column=3)
 
-addButton = tk.Button(root, text="Add", padx=45, command=journal)
+addButton = tk.Button(root, text="Add", padx=45, command=add)
 addButton.grid(row=0,column=4)
 
-# Database
-
-# Create a database or connect to one
-conn = sqlite3.connect('price_alert.db')
-
-# Create a cursor
-c = conn.cursor()
-
-# Create a table
-
-c.execute("""CREATE TABLE prices (
-    symbol text,
-    target_price real,
-    current_price real
-    ) """
-          )
+query_btn = tk.Button(root, text="Query", command=query)
+query_btn.grid(row=0, column=5, columnspan=2)
 
 
+tree = ttk.Treeview(root, column=("c1", "c2"), show='headings')
+tree.column("#1", anchor=tk.CENTER)
 
-# Commit changes
-conn.commit()
+tree.heading("#1", text="SYMBOL")
 
-# Close connection
-conn.close()
+tree.column("#2", anchor=tk.CENTER)
 
+tree.heading("#2", text="TARGET")
 
-
-
-# height = 5
-# width = 3
-# for i in range(1, height): #Rows
-#     for j in range(width): #Columns
-#         if (i == 1): # header of the table
-#             if j == 0:
-#                 symbol_header = tk.Label(root, text="Symbol")
-#                 symbol_header.grid(row=i, column=j)
-#                 continue
-#             if j == 1:
-#                 target = tk.Label(root, text="Target Price")
-#                 target.grid(row=i, column=j)
-#                 continue
-#             if j == 2:
-#                 current = tk.Label(root, text="Current Price")
-#                 current.grid(row=i, column=j)
-#                 continue
-
-#         content = tk.Label(root, text="abc")
-#         content.grid(row=i, column=j)
+tree.grid(row=2, column=3)
 
 
 
